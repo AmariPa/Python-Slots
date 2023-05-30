b52c941999d681732719103ffc23ae074af18d44
Amari Pascoe <elliotpascoe04@gmail.com>
2023-05-30 15:41:25 -0400

    added slots and images; must resize images; fix slots

--- main.py
+++ main.py
@@ -1,6 +1,60 @@
+# imports
+import random
+import climage
+from PIL import Image
+
+# consts
 MAX_LINES = 3
 MAX_BET = 500
 MIN_BET = 1
+ROWS = 3
+COLS = 3
+
+# images for slots
+A = climage.convert('apple.png')
+B = climage.convert('banana.png')
+C = climage.convert('cherry.png')
+D = climage.convert('orange.png')
+
+# assigns images a value
+symbol_count = {
+    A: 5,
+    B: 3,
+    C: 4,
+    D: 5,
+}
+
+
+def get_slot_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+# prints the slot machine in the CLI
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], '|')
+            else:
+                print(column[row])
+
 
 # Asks user how much money they want to deposit. Requires a number
 
@@ -64,13 +118,15 @@ def main():
 
         if total_bet > balance:
             print(
-                f"This bet exceeds your balance, your current balance is ${balance}")
+                f"This bet exceeds your balance, your current balance is: ${balance}")
         else:
             break
-    bet = get_bet()
-    total_bet = bet * lines
     print(
         f'You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}.')
+
+# generates slot machine
+    slots = get_slot_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
